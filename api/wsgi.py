abeaed82860dc80150be39cb4773538880357ce1
JonathanRosas14 <tijonathanrosas01@gmail.com>
2026-07-06 09:18:27 -0500

    debug: test includeFiles with simple WSGI

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,10 +1,16 @@
-import os
-import sys
+import os, sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent / 'Backend'))
+backend = Path(__file__).resolve().parent.parent / 'Backend'
+sys.path.insert(0, str(backend))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(status, headers)
+    body = [b'Backend exists: ']
+    body.append(str(backend.exists()).encode())
+    if backend.exists():
+        body.append(b'\nContents: ')
+        body.append(str([str(p.name) for p in backend.iterdir()]).encode())
+    return body

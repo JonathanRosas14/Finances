a823fbff6e21048c58aaddf783da77047bb81e05
JonathanRosas14 <tijonathanrosas01@gmail.com>
2026-07-06 09:11:25 -0500

    debug: test WSGI to check includeFiles

--- api/wsgi.py
+++ api/wsgi.py
@@ -2,9 +2,16 @@ import os
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent / 'Backend'))
+# Check if Backend dir exists
+backend_dir = str(Path(__file__).resolve().parent.parent / 'Backend')
+sys.path.insert(0, backend_dir)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+def application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+    start_response(status, headers)
+    body = []
+    body.append(f'Python: {sys.version}'.encode())
+    body.append(f'\nPath: {backend_dir}'.encode())
+    body.append(f'\nFiles: {os.listdir(backend_dir) if os.path.exists(backend_dir) else "NOT FOUND"}'.encode())
+    return body
